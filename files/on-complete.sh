09bbd21df7bb6dd43ec0693519a7ece9260e7532
0612me <0612me@users.noreply.github.com>
2017-07-18 20:29:50 +0800

    Update on-complete.sh

--- files/on-complete.sh
+++ files/on-complete.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-if [ $2 -eq 1 ]; then
-	rclone --config=/conf/rclone.conf copy /data/_dl gdrive:rclone
-	mv "$3" /data/_waste
-	rm -f /data/_waste/*
-fi
-echo [$(date)] $2, $3, $1 "<br>" >> /data/_log.html
+rclone --config=/conf/rclone.conf copy /data/_dl gdrive:rclone
+        mv /data/_dl/* /data/_waste/
+        rm -f /data/_waste/*
